a4544786f2ed0458d09efe39084f09902fd7245c
Wing Ho Yeung <ho13102003@hotmail.com>
2013-04-22 19:29:56 +0800

    UPDATED

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
 <body>
 	This is the start page of the bigtwo project.
+	This is the second page of the bigtwo project.
+	his is the second page of the bigtwo project.
+	DEAL!
 </body>
 </html>
